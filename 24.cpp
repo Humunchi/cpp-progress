38f77c35cec48b1e431e67e213167a9bfff367f7
Humunchi <humunchi>
2021-03-17 19:51:21 +0200

    adding new file test

---
+++ 24.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+//REHUMILE BIAGINI PRACTICE (single line comment)
+/* CPP MULTILINE COMMENT
+
+]]]]]A task takes up the amount of time allocated to its completeion.]]]]]]]
+
+
+1. writing 1st program
+2. Organising parts of Program
+3. Creating VAr and COnst
+4. Using Expressions, Statements and Operators
+5. Calling Funtions
+6. Controlling flow of the program
+7. Storing info in Array and Strings
+
+*/
+
+/*1. writing 1st program 
+int main()
+{
+    std::cout << "Something. \n";
+    return 0;
+}
+*/
+
+//2.
+
